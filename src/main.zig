afc8b2c7f236ee7664c28f95f2608f9bf9d25b5c
ikubaku <hide4d51@gmail.com>
2021-07-23 05:13:18 +0900

    Implement LED blinker sample

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,25 @@
 const gpio = @import("hal/gpio.zig");
 const sio = @import("hal/sio.zig");
+const reset = @import("hal/reset.zig");
 
 pub fn hako_main() noreturn {
+    reset.deassert_reset_of(.IO_BANK0);
+
     gpio.set_dir(.P25, .Output);
     gpio.set_function(.P25, .F5);
 
     sio.set_output_enable(.GPIO25);
-    sio.set_output(.GPIO25);
 
     while (true) {
-        //gpio.led_on();
-        //uart_api.gets(uart0, &recv_buf, RECV_BUFSIZE);
+        sio.set_output(.GPIO25);
+        busy_wait();
 
-        //gpio.led_off();
-        //uart_api.println(uart0, &recv_buf, RECV_BUFSIZE);
+        sio.clear_output(.GPIO25);
+        busy_wait();
     }
+}
+
+fn busy_wait() void {
+    var i: usize = 0;
+    while (i < 100_000) : (i += 1) {}
 }
